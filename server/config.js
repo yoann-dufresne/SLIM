b03f6c2d8ceadafe2debbda15a28fa37ab7dbe73
yoann-dufresne <yoann.dufresne0@gmail.com>
2017-09-06 13:51:54 +0200

    add a config file containing all sensible values that will not be commited later

---
+++ server/config.js
@@ -0,0 +1,10 @@
+
+exports.mailer = {
+	host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+        user: 'username',
+        pass: 'password'
+    }
+}
