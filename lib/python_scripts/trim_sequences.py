75d56def1bb86722561c6bb16ce46f16f8681fe1
trtcrd <tristan.cordier@laposte.net>
2018-02-16 15:57:50 +0100

    modfied the trimming lib to allow full fasta as input and write the output in a fasta too

--- lib/python_scripts/trim_sequences.py
+++ lib/python_scripts/trim_sequences.py
@@ -1,6 +1,8 @@
 #### Triming sequences based on a particular motif
 
 import argparse
+from Bio import SeqIO
+
 
 def trim_before(motif, seq):
 	idx = seq.find(motif)
@@ -31,22 +33,25 @@ def trim_motif(motif, seq):
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--trim_position', '-tp', help='what to trim, before / after the motif, or remove the motif itself. Possible values: before, after, motif')
-	parser.add_argument('--sequence', '-s', help='the sequence to search motif in')
-	parser.add_argument('motifs', type=str, nargs='+',help='a list of motif(s) to search for in the sequence')
+	parser.add_argument('--fasta', '-f', help='the fasta containing the sequences to be trimmed with the motif')
+	parser.add_argument('--motif', '-m', help='the motif to search in the sequence')
+	parser.add_argument('--output', '-o', help='output fasta filename')
 	args = parser.parse_args()
 	
-	for motif in args.motifs:
-		if args.trim_position == "before":
-			print(trim_before(motif, args.sequence))
-		elif args.trim_position == "after":
-			print(trim_after(motif, args.sequence))
-		elif args.trim_position == "motif":
-			print(trim_motif(motif, args.sequence))
-		else:
-			print("\nWrong argument given for trim position\n")
-			parser.print_help()
-	
-	
+	with open(str(args.output), 'w') as fw:
+		for record in SeqIO.parse(args.fasta, "fasta"):
+			fw.write('>' + record.description + '\n')
+			if args.trim_position == "before":
+				fw.write(str(trim_before(args.motif, record.seq) + '\n'))
+			elif args.trim_position == "after":
+				fw.write(trim_after(args.motif, record.seq) + '\n')
+			elif args.trim_position == "motif":
+				fw.write(trim_motif(args.motif, record.seq) + '\n')
+			else:
+				print("\nWrong argument given for trim position\n")
+				parser.print_help()
+				exit(1)
+
 
 if __name__ == "__main__":
 	main()
