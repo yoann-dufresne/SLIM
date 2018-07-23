d50479d3931fc41e58f3abbca7928fd3cf2559e1
trtcrd <tristan.cordier@laposte.net>
2018-07-23 11:01:42 +0200

    update get_dependecies script to v1.1

--- get_dependencies_slim_v1.0.sh
+++ get_dependencies_slim_v1.0.sh
@@ -11,36 +11,39 @@ cd lib/
 if [ ! -d "jquery-autocomplete" ]; then
 	mkdir jquery-autocomplete
 	# git clone https://github.com/devbridge/jQuery-Autocomplete.git jquery-autocomplete
-else
 	cd jquery-autocomplete
 	# git pull
 	curl -OL https://github.com/devbridge/jQuery-Autocomplete/archive/v1.4.7.tar.gz
 	tar -xzvf v1.4.7.tar.gz
 	mv jQuery-Autocomplete-1.4.7/* .
 	cd ..
+else
+	echo "jQuery already there..."
 fi
 
 # Papaparser
 if [ ! -d "papa" ]; then
 	mkdir papa
 	#git clone https://github.com/mholt/PapaParse.git papa
-else
 	cd papa
 	#git pull
 	curl -OL https://github.com/mholt/PapaParse/archive/4.4.0.tar.gz
 	tar -xzvf 4.4.0.tar.gz
 	mv PapaParse-4.4.0/* .
 	cd ..
+else
+	echo "PAPAparser already there..."
 fi
 
 
 # Demultiplexing tool # no stable release yet... oops
 if [ ! -d "DTD" ]; then
 	git clone https://github.com/yoann-dufresne/DoubleTagDemultiplexer.git DTD/
-else
 	cd DTD
 	git pull
 	cd ..
+else
+	echo "DTD is already there..."
 fi
 
 
@@ -48,13 +51,14 @@ fi
 if [ ! -d "pandaseq" ]; then
 	mkdir pandaseq
 	#git clone https://github.com/neufeld/pandaseq.git pandaseq/
-else
 	cd pandaseq
 	#git pull
 	curl -OL https://github.com/neufeld/pandaseq/archive/v2.11.tar.gz
 	tar -xzvf v2.11.tar.gz
 	mv pandaseq-2.11/* .
 	cd ..
+else
+	echo "PANDAseq is already there..."
 fi
 
 
@@ -62,13 +66,14 @@ fi
 if [ ! -d "vsearch" ]; then
 	mkdir vsearch
 	# git clone https://github.com/torognes/vsearch.git vsearch/
-else
 	cd vsearch
 	curl -OL https://github.com/torognes/vsearch/archive/v2.8.0.tar.gz
 	tar -xzvf v2.8.0.tar.gz
 	mv vsearch-2.8.0/* .
 	# git pull
 	cd ..
+else
+	echo "VSEARCH is already there..."
 fi
 
 
@@ -87,11 +92,12 @@ cd ..
 if [ ! -d "swarm" ]; then
 	mkdir swarm
 	# git clone https://github.com/torognes/swarm.git swarm/
-else
 	cd swarm
 	#git pull
 	curl -OL https://github.com/torognes/swarm/archive/v2.2.2.tar.gz
 	tar -xzvf v2.2.2.tar.gz
 	mv swarm-2.2.2/* .
 	cd ..
+else
+	echo "SWARM is already there..."
 fi
