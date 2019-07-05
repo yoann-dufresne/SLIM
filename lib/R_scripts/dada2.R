acd26a65f24bd8c37a639481c30c602763a85e94
trtcrd <tristan.cordier@laposte.net>
2019-07-05 12:21:33 +0200

    stupid fix rbind

--- lib/R_scripts/dada2.R
+++ lib/R_scripts/dada2.R
@@ -146,7 +146,7 @@ if (length(noReads) > 0)
 }
 
 # add the samples with no reads in the ASV table
-ASV_table_consensus <- cbind(ASV_table_consensus, tmp)
+ASV_table_consensus <- rbind(ASV_table_consensus, tmp)
 
 # transpose and sort as in the t2s
 ASV_table_consensus <- t(ASV_table_consensus[as.character(t2s$sample),])
