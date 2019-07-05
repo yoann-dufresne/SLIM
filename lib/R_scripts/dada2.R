5a6a91c6d359deb776850e406caf34e8221f0f52
trtcrd <tristan.cordier@laposte.net>
2019-07-05 11:12:13 +0200

    handle if empty fastq after filtering and trim function

--- lib/R_scripts/dada2.R
+++ lib/R_scripts/dada2.R
@@ -1,6 +1,5 @@
 
 args <- commandArgs(TRUE)
-print(args)
 
 by_lib <- args[1]
 t2s_file <- args[2]
@@ -13,7 +12,6 @@ rev <- args[8]
 
 require(dada2)
 require(seqinr)
-
 
 ## general path
 setwd(paste("/app/data/", token, "/", sep=""))
@@ -43,25 +41,30 @@ path.filt <- file.path(path, "filterAndTrimed")
 filtFs <- file.path(path.filt, basename(fnFs))
 filtRs <- file.path(path.filt, basename(fnRs))
 filtering <- filterAndTrim(fwd=fnFs, rev=fnRs, filt=filtFs, filt.rev=filtRs, multithread=cpus, verbose=TRUE)
+filtering <- cbind(sample_ID = rownames(filtering), filtering)
+write.table(filtering, file = paste0(t2s_name, "_filtering.tsv"), quote = F, sep="\t", row.names = F, fileEncoding = "UTF-8")
 
-##### filtering??
+# extract samples with no reads if any
+noReads <- rownames(filtering[filtering[,"reads.out"]==0,])
+noReads <- sub("_fwd.fastq", "", noReads)
+
 ##keep only samples with reads
-#keep <- filtering[,"reads.out"] > 100
+keep <- filtering[,"reads.out"] > 0
 ### file path
-#filtFs_kept <- filtFs[keep]
-#filtRs_kept <- filtRs[keep]
+filtFs_kept <- filtFs[keep]
+filtRs_kept <- filtRs[keep]
 
 # DADA2 workflow
 message("Learning error model be passed to DADA2")
 message(paste("###", length(lib_list)), " models to learn")
 # to collect processed samples
-names_list <- c()
+#names_list <- c()
 
 for (i in lib_list)
 {
   # fetch here either the list of sample to be processed, or the unique sample from the list
-  assign(paste("filtFs_",i, sep=""), filtFs[grep(paste(i, "_", sep=""), filtFs)])
-  assign(paste("filtRs_",i, sep=""), filtRs[grep(paste(i, "_", sep=""), filtRs)])
+  assign(paste("filtFs_",i, sep=""), filtFs_kept[grep(paste(i, "_", sep=""), filtFs_kept)])
+  assign(paste("filtRs_",i, sep=""), filtRs_kept[grep(paste(i, "_", sep=""), filtRs_kept)])
   # to ensure reproducibility
   set.seed(100)
   assign(paste("errFWD_",i, sep=""), learnErrors(get(paste("filtFs_",i, sep="")), nbases = 1e8, multithread=cpus, randomize=TRUE))
@@ -88,7 +91,7 @@ for (i in lib_list)
     suffix <- "_fwd.fastq"
     name <- sub(prefix, "", name)
     name <- sub(suffix, "", name)
-    names_list <- c(names_list, name)
+    #names_list <- c(names_list, name)
     saveRDS(merger, file.path(path, paste0(name, "_merger.rds")))
     message(paste(j, "/", length(filtFs_n), " sample ", name, " is done for library: ", i, sep=""))
   }
@@ -118,15 +121,35 @@ ASV_table_consensus <- removeBimeraDenovo(ASV_table, method = "consensus", multi
 
 # Now extract the ASV sequences and paste them to make a fasta
 ASV_headers <- paste0("ASV", c(1:ncol(ASV_table_consensus)))
-seqs <-getSequences(ASV_table_consensus)
+seqs <- getSequences(ASV_table_consensus)
 write.fasta(as.list(seqs), ASV_headers, repseq_file)
 
 # now paste the ASV instead of sequences in the matrix before writing it, sort it to match the t2s and transpose
 colnames(ASV_table_consensus) <- ASV_headers
-# collect the processed sample in the order as in the t2s_file
-samples_order <- subset(as.character(t2s$sample), t2s$sample %in% names_list)
 
-ASV_table_consensus <- t(ASV_table_consensus[samples_order,])
+# collect the processed sample in the order as in the t2s_file
+#samples_order <- subset(as.character(t2s$sample), t2s$sample %in% names_list)
+
+# create empty vector for samples with no reads
+if (length(noReads) > 0)
+{
+  tmp <- c()
+  for (i in 1:length(noReads))
+  {
+    noReads[i] <- sub(paste0(t2s_name, "_"), "", noReads[i])
+    noReads[i] <- sub("_fwd.fastq", "", noReads[i])
+    # remove the lib ID from noReads
+    for (j in unique(t2s$run)) noReads[i] <- sub(paste0(j, "_"), "", noReads[i])
+    tmp <- rbind(tmp, rep(0, length(ASV_headers)))
+  }
+  rownames(tmp) <- noReads
+}
+
+# add the samples with no reads in the ASV table
+ASV_table_consensus <- cbind(ASV_table_consensus, tmp)
+
+# transpose and sort as in the t2s
+ASV_table_consensus <- t(ASV_table_consensus[as.character(t2s$sample),])
 ASV_table_consensus <- data.frame(cbind(ASV_ID = ASV_headers, ASV_table_consensus))
 # finally
 write.table(ASV_table_consensus, file = asv_table, quote = F, sep="\t", row.names = F, fileEncoding = "UTF-8")
